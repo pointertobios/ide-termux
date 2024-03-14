8e11e6aea058051de9a7b1e0a01b1d94f71f8221
pointer-to-bios <pointer-to-bios@outlook.com>
2024-03-14 13:03:53 +0000

    dir show

--- src/components/project_viewer.rs
+++ src/components/project_viewer.rs
@@ -6,6 +6,7 @@ use crossterm::{
     style::{self, Color},
 };
 use std::{
+    fs,
     io::Stdout,
     process::exit,
     sync::{Arc, RwLock},
@@ -15,6 +16,8 @@ pub struct ProjectViewer {
     container: Arc<RwLock<Container>>,
     path: String,
     at_line: usize,
+
+    fs: Filesystem,
 }
 
 impl ProjectViewer {
@@ -22,10 +25,12 @@ impl ProjectViewer {
         let mut container = Container::new("ProjectViewer", None);
         container.focus();
         let container = Arc::new(RwLock::new(container));
+        let path = std::env::var("PWD").unwrap();
         let res = Arc::new(RwLock::new(ProjectViewer {
             container,
-            path: std::env::var("PWD").unwrap(),
+            path: path.clone(),
             at_line: 0,
+            fs: Filesystem::new(path),
         }));
         res.read()
             .unwrap()
@@ -54,7 +59,7 @@ impl ProjectViewer {
                             }
                         }
                         KeyCode::Down => {
-                            if line < contsize.1 - 2 {
+                            if line < (contsize.1 - 2).min(res_ref.read().unwrap().fs.get(contsize.1 - 1).len()-1) {
                                 res_ref.write().unwrap().at_line += 1;
                             }
                         }
@@ -136,6 +141,7 @@ impl Component for ProjectViewer {
                 queue!(stdout, style::Print(" ".to_string()),).unwrap();
             }
             // 主体
+	    let content = self.fs.get(size.1);
             queue!(
                 stdout,
                 cursor::MoveTo(offset.0 as u16, offset.1 as u16 + 1),
@@ -143,7 +149,7 @@ impl Component for ProjectViewer {
             )
             .unwrap();
             let mut light_line = true;
-            for i in 0..(size.1 - 1) {
+            for i in 0..(size.1 - 1).min(content.len()) {
                 if i == self.at_line {
                     queue!(
                         stdout,
@@ -154,9 +160,16 @@ impl Component for ProjectViewer {
                 } else if light_line {
                     queue!(stdout, style::ResetColor).unwrap();
                 } else {
-                    queue!(stdout, style::SetBackgroundColor(Color::DarkGrey)).unwrap();
+                    queue!(stdout, style::ResetColor, style::SetBackgroundColor(Color::DarkGrey)).unwrap();
                 }
-                let len = 0;
+		let line = content[i].clone();
+                let line = if line.len() > size.0 {
+		    line.split_at(size.0).0.to_string()
+		} else {
+		    line
+		};
+		let len = line.len();
+		queue!(stdout, style::Print(line)).unwrap();
                 for _ in 0..(size.0 - len) {
                     queue!(stdout, style::Print(" ".to_string())).unwrap();
                 }
@@ -164,5 +177,75 @@ impl Component for ProjectViewer {
                 light_line = !light_line;
             }
         }
+    }
+}
+
+enum PathType {
+    File,
+    Directory,
+    SymLink,
+    None,
+}
+
+struct Path(String, PathType, Vec<Box<Self>>);
+
+impl PartialEq for Path {
+    fn eq(&self, s: &Self) -> bool {
+	self.0 == s.0
+    }
+}
+
+impl PartialOrd for Path {
+    fn partial_cmp(&self, s: &Self) -> Option<Orderibg> {}
+}
+
+struct Filesystem {
+    root: String,
+    path_cache: Vec<Path>,
+    showing_start: usize,
+}
+
+impl Filesystem {
+    pub fn new(root: String) -> Self {
+        let mut res = Filesystem {
+            root,
+            path_cache: Vec::new(),
+	    showing_start: 0,
+        };
+        for entry in fs::read_dir(&res.root).unwrap() {
+            let entry = entry.unwrap();
+            let ptype = if entry.path().is_file() {
+                PathType::File
+            } else if entry.path().is_dir() {
+                PathType::Directory
+            } else if entry.path().is_symlink() {
+                PathType::SymLink
+            } else {
+                PathType::None
+            };
+            res.path_cache.push(Path(
+                entry.file_name().into_string().unwrap(),
+                ptype,
+                Vec::new(),
+            ));
+        }
+        res
+    }
+
+    pub fn get(&self, lines: usize) -> Vec<String> {
+	let mut count = 0;
+	let mut line = 0;
+	let mut res = Vec::new();
+	for entry in &self.path_cache {
+	    if line >= self.showing_start {
+		res.push(entry.0.clone());
+		count += 1;
+	    }
+	    line += 1;
+	    if count >= lines {
+		break;
+	    }
+	}
+	res
     }
 }
